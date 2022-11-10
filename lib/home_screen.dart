fb75d215c38eec46c7bdbd44f88a7cd414ea0f30
ginohmk <michotall95@gmail.com>
2022-11-10 14:58:12 +0100

    feat: CRUD implemented

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,16 +1,77 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:navigation_task_hng/login_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final player = TextEditingController();
+  final update = TextEditingController();
+
+  final firestore = FirebaseFirestore.instance;
+
+  late Stream<QuerySnapshot> streamUser;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    streamUser = firestore.collection('users').snapshots();
+  }
+
+  void _clearField() {
+    player.clear();
+    update.clear();
+  }
+
+  void _create(playerName) async {
+    try {
+      await firestore
+          .collection('users')
+          .doc('testUser')
+          .set({'playerName': playerName});
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  void _update(data) async {
+    try {
+      firestore.collection('users').doc('testUser').set({'playerName': data});
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  void _delete() async {
+    try {
+      firestore.collection('users').doc('testUser').delete();
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    player.dispose();
+    // playerUpdate.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+
     return Scaffold(
         appBar: AppBar(
           title: Text('Home Screen'),
@@ -49,8 +110,129 @@ class HomeScreen extends StatelessWidget {
                 SizedBox(
                   height: 20.0,
                 ),
+                Text(
+                  'Enter Player name',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  width: size.width * 0.8,
+                  decoration: BoxDecoration(
+                      color: Colors.indigo[200],
+                      borderRadius: BorderRadius.circular(20)),
+                  child: TextField(
+                    controller: player,
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.lock_clock_outlined,
+                        color: Colors.indigo,
+                      ),
+                      border: InputBorder.none,
+                      hintText: "player name",
+                    ),
+                  ),
+                ),
                 ElevatedButton(
-                    onPressed: () {
+                  onPressed: () {
+                    _create(player.text);
+                    _clearField();
+                  },
+                  style: ElevatedButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 40)),
+                  child: Text('Create Player'),
+                ),
+                SizedBox(height: 20.0),
+                StreamBuilder<QuerySnapshot>(
+                  stream: streamUser,
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    var items = [];
+
+                    if (snapshot.hasError) {
+                      return Text('No player added');
+                    }
+
+                    if (snapshot.hasData) {
+                      //get the data
+                      QuerySnapshot querySnapshot = snapshot.data;
+                      List<QueryDocumentSnapshot> documents =
+                          querySnapshot.docs;
+
+                      //Convert the documents to Maps
+                      items = documents
+                          .map((e) => {'playerName': e['playerName']})
+                          .toList();
+                    }
+
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'PLayer Name: ',
+                          style: TextStyle(
+                              fontSize: 25.0, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          items.isNotEmpty
+                              ? items[0]['playerName'].toString()
+                              : "No Player added",
+                          style: TextStyle(
+                              fontSize: 25.0, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  width: size.width * 0.8,
+                  decoration: BoxDecoration(
+                      color: Colors.indigo[200],
+                      borderRadius: BorderRadius.circular(20)),
+                  child: TextField(
+                    controller: update,
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.lock_clock_outlined,
+                        color: Colors.indigo,
+                      ),
+                      border: InputBorder.none,
+                      hintText: "Update name",
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _update(update.text);
+                    _clearField();
+                  },
+                  style: ElevatedButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 40)),
+                  child: Text('Update Player'),
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    _delete();
+                  },
+                  style: ElevatedButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 40)),
+                  child: Text('Delete PLayer'),
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                    onPressed: () async {
                       FirebaseAuth.instance.signOut().then((value) => {
                             debugPrint('Signed out successfully'),
                             Navigator.pushReplacement(context,
@@ -63,6 +245,9 @@ class HomeScreen extends StatelessWidget {
                         padding:
                             EdgeInsets.symmetric(vertical: 15, horizontal: 40)),
                     child: Text('Logout')),
+                SizedBox(
+                  height: 20.0,
+                ),
               ],
             ),
           ),
